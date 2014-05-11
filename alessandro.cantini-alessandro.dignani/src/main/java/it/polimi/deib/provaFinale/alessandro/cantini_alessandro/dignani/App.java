320634f96056f4dc30c2897d0ac7e3847326a7b9
alessandrocantini <alessandro.cantini@mail.polimi.it>
2014-05-12 00:00:00 +0200

    first

---
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/alessandro/cantini_alessandro/dignani/App.java
@@ -0,0 +1,13 @@
+package it.polimi.deib.provaFinale.alessandro.cantini_alessandro.dignani;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Ciao Mondo!" );
+    }
+}
