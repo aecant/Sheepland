f6ceaa70c1d462d3247aa66510bb43593cdd5284
alessandrodignani <alessandro.dignani@mail.polimi.it>
2014-05-15 22:11:39 +0200

    Implementazione Gregge

--- alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/alessandro_cantini_alessandro_dignani/model/Gregge.java
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/alessandro_cantini_alessandro_dignani/model/Gregge.java
@@ -6,6 +6,10 @@ public class Gregge {
 	private static Gregge istanza = null;
 	ArrayList<Pecora> pecore;
 	
+	public ArrayList<Pecora> getPecore() {
+		return pecore;
+	}
+
 	private Gregge() {
 		pecore = new ArrayList<Pecora>();
 	}
@@ -15,9 +19,20 @@ public class Gregge {
 			istanza = new Gregge();
 		return istanza;
 	}
-
-	public static void trasformaAgnelloInPecora(Agnello agnello) {
-		// TODO Auto-generated method stub
-		
+	
+	public void inserisci(Pecora pec) {
+		pecore.add(pec);
+	}
+	
+	public void elimina(Pecora pec) {
+		pecore.remove(pec);
+	}
+	
+	/**
+	 * Trasforma l'agnello passato come parametro in pecora con un casting
+	 * @param agnello
+	 */
+	public void trasformaAgnelloInPecora(Agnello agnello) {
+		pecore.set(pecore.indexOf(agnello), (Pecora) agnello);
 	}
 }
--- alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/alessandro_cantini_alessandro_dignani/model/Partita.java
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/alessandro_cantini_alessandro_dignani/model/Partita.java
@@ -1,10 +1,5 @@
 package it.polimi.deib.provaFinale.alessandro_cantini_alessandro_dignani.model;
 
 public class Partita {
-
-	public static void trasformaAgnelloInPecora(Agnello agnello) {
-		// TODO Auto-generated method stub
-		
-	}
 	
 }
