35db0ab3954075c3d873f3a7d0093456b634ec82
alessandrocantini <alessandro.cantini@mail.polimi.it>
2014-05-16 11:01:21 +0200

    Gregge e Mazzo non sono più singleton

--- alessandro.cantini-alessandro.dignani/src/test/java/it/polimi/deib/provaFinale/alessandro_cantini_alessandro_dignani/model/MazzoTest.java
+++ alessandro.cantini-alessandro.dignani/src/test/java/it/polimi/deib/provaFinale/alessandro_cantini_alessandro_dignani/model/MazzoTest.java
@@ -1,29 +1,22 @@
 package it.polimi.deib.provaFinale.alessandro_cantini_alessandro_dignani.model;
 
-import static org.junit.Assert.*;
 import it.polimi.deib.provaFinale.alessandro_cantini_alessandro_dignani.model.Mazzo;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class MazzoTest {
-	Mazzo mazzo1, mazzo2;	
+	Mazzo mazzo;	
 	
 	@Before
 	public void setUp(){
-		mazzo1 = Mazzo.getMazzo();
-		mazzo2 = Mazzo.getMazzo();
+		mazzo = new Mazzo();
 	}
 	
-	
-	@Test
-	public void testMazzo() {
-		assertEquals(mazzo1, mazzo2);
-	}
 
 	@Test (expected=IllegalArgumentException.class)
 	public void testPrelevaCarta() {
 		for(int i=0; i<Costanti.MAX_VALORE_TESSERA+2; i++)
-			Mazzo.getMazzo().prelevaCarta(TipoTerritorio.BOSCO);
+			mazzo.prelevaCarta(TipoTerritorio.BOSCO);
 	}
 }
