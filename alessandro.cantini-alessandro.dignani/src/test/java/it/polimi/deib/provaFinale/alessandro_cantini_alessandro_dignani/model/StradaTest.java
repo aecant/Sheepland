018d7b477b92c10f51d80b565699e224e61b5069
alessandrocantini <alessandro.cantini@mail.polimi.it>
2014-05-16 08:40:18 +0200

    aggiornato test Strada

--- alessandro.cantini-alessandro.dignani/src/test/java/it/polimi/deib/provaFinale/alessandro_cantini_alessandro_dignani/model/StradaTest.java
+++ alessandro.cantini-alessandro.dignani/src/test/java/it/polimi/deib/provaFinale/alessandro_cantini_alessandro_dignani/model/StradaTest.java
@@ -7,25 +7,27 @@ import org.junit.Test;
 
 public class StradaTest {
 	Strada s1, s2, s3;
-	
-	@Before	
+
+	@Before
 	public void setUp() {
-		Territorio t1 = new Territorio (4, TipoTerritorio.DESERTO);
-		Territorio t2 = new Territorio (5, TipoTerritorio.DESERTO);
-		Territorio t3 = new Territorio (7, TipoTerritorio.LAGO);
-		
+		Territorio t1 = new Territorio(4, TipoTerritorio.DESERTO);
+		Territorio t2 = new Territorio(5, TipoTerritorio.DESERTO);
+		Territorio t3 = new Territorio(7, TipoTerritorio.LAGO);
+
 		s1 = new Strada(t1, t2);
 		s2 = new Strada(t2, t1);
 		s3 = new Strada(t1, t3);
 	}
-	
+
 	@Test
 	public void testEquals() {
-		assertTrue(s1.equals(s2));
+		assertEquals(s1, s1);
+		assertEquals(s1, s2);
 		assertFalse(s1.equals(s3));
 	}
-	@Test (expected=IllegalArgumentException.class)
+
+	@Test(expected = IllegalArgumentException.class)
 	public void testStrada() {
-		new Strada (null, new Territorio(4, TipoTerritorio.DESERTO));
+		new Strada(null, new Territorio(4, TipoTerritorio.DESERTO));
 	}
 }
