aeeec6a48e0fd9645ce24c6d7e75fda6374dfc46
alessandrocantini <alessandro.cantini@mail.polimi.it>
2014-05-17 12:53:11 +0200

    Aggiunto metodo sonoContigue() in Mappa

--- alessandro.cantini-alessandro.dignani/src/test/java/it/polimi/deib/provaFinale/alessandro_cantini_alessandro_dignani/model/MappaTest.java
+++ alessandro.cantini-alessandro.dignani/src/test/java/it/polimi/deib/provaFinale/alessandro_cantini_alessandro_dignani/model/MappaTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class MappaTest {
 	
 	Territorio t4, t5, t6, t7;
-	Strada s1, s2, s3, s4, s5;
+	Strada s1, s6, s2, s5, s4;
 
 	
 	@Before
@@ -20,11 +20,12 @@ public class MappaTest {
 		t7 = new Territorio(7, TipoTerritorio.LAGO);
 		
 		s1 = new Strada(t4,t7);
-		s2 = new Strada(t4,t5);
-		s3 = new Strada(t5,t7);
-		s4 = new Strada(t5,t6);
-		s5 = new Strada(t6,t7);
+		s6 = new Strada(t4,t5);
+		s2 = new Strada(t5,t7);
+		s5 = new Strada(t5,t6);
+		s4 = new Strada(t6,t7);
 	}
+	
 	
 	
 	@Test
@@ -34,7 +35,26 @@ public class MappaTest {
 	}
 
 	@Test
+	public void testSonoContigue() {
+		assertTrue(Mappa.getMappa().sonoContigue(s1, s6));
+		assertTrue(Mappa.getMappa().sonoContigue(s6, s1));
+		assertTrue(Mappa.getMappa().sonoContigue(s1, s2));
+		assertTrue(Mappa.getMappa().sonoContigue(s2, s4));
+		assertTrue(Mappa.getMappa().sonoContigue(s4, s5));
+		assertTrue(Mappa.getMappa().sonoContigue(s2, s5));
+
+		assertFalse(Mappa.getMappa().sonoContigue(s1, s5));
+		assertFalse(Mappa.getMappa().sonoContigue(s5, s1));
+		assertFalse(Mappa.getMappa().sonoContigue(s1, s4));
+		assertFalse(Mappa.getMappa().sonoContigue(s6, s5));
+		
+		
+	}
+	
+	@Test
 	public void testSonoConfinanti() {
 		assertTrue(Mappa.getMappa().sonoConfinanti(t4, t5));
+		assertTrue(Mappa.getMappa().sonoConfinanti(t4, t7));
+		assertFalse(Mappa.getMappa().sonoConfinanti(t4, t6));
 	}
 }
