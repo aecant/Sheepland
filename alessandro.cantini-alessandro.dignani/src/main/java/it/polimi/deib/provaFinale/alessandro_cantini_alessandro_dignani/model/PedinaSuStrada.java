92130a1a2076551392ef1c720394a9d68d091e0c
alessandrodignani <alessandro.dignani@mail.polimi.it>
2014-05-17 13:36:23 +0200

    Aggiunto metodo che controlla se una strada è occupata

--- alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/alessandro_cantini_alessandro_dignani/model/PedinaSuStrada.java
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/alessandro_cantini_alessandro_dignani/model/PedinaSuStrada.java
@@ -1,10 +1,26 @@
 package it.polimi.deib.provaFinale.alessandro_cantini_alessandro_dignani.model;
 
+import java.util.List;
+
 public class PedinaSuStrada {
 	private Strada strada;
 
 	public PedinaSuStrada(Strada strada) {
 		this.strada = strada;
+	}
+	
+	/**
+	 * Data una strada ed una lista di pedine su strada controlla se la strada è occupata da una di quelle pedine
+	 * 
+	 * @param lista
+	 * @param strada
+	 * @return
+	 */
+	public static boolean stradaOccupata(List<PedinaSuStrada> lista, Strada strada) {
+		for(PedinaSuStrada p : lista)
+			if(p.getStrada().equals(strada))
+				return true;
+		return false;
 	}
 
 	public Strada getStrada() {
