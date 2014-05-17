cdf8b5bfcaf85d5b2d251c45d80d033da9b2ca53
alessandrodignani <alessandro.dignani@mail.polimi.it>
2014-05-17 16:13:36 +0200

    Implementato il metodo statico territorioOccupato in Animale

--- alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/alessandro_cantini_alessandro_dignani/model/Animale.java
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/alessandro_cantini_alessandro_dignani/model/Animale.java
@@ -1,5 +1,7 @@
 package it.polimi.deib.provaFinale.alessandro_cantini_alessandro_dignani.model;
 
+import java.util.List;
+
 public class Animale {
 	private Territorio posizione;
 
@@ -9,6 +11,21 @@ public class Animale {
 
 	public void muoviIn(Territorio territorio) {
 		posizione = territorio;
+	}
+
+	
+	/**
+	 * Data una strada ed una lista di pedine su strada controlla se la strada è occupata da una di quelle pedine
+	 * 
+	 * @param lista
+	 * @param territorio
+	 * @return
+	 */
+	public static boolean territorioOccupato(List<Animale> lista, Territorio territorio) {
+		for(Animale a : lista)
+			if(a.getPosizione().equals(territorio))
+				return true;
+		return false;
 	}
 
 	public Territorio getPosizione() {
