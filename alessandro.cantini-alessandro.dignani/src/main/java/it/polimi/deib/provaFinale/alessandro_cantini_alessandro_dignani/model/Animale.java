8758729595d5f377d5f1dbe55ae64b68cf42d169
alessandrocantini <alessandro.cantini@mail.polimi.it>
2014-05-18 14:53:59 +0200

    Eliminato metodo territorioOccupato() in Animale Eliminato metodo stradaOccupata() in Strada

--- alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/alessandro_cantini_alessandro_dignani/model/Animale.java
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/alessandro_cantini_alessandro_dignani/model/Animale.java
@@ -1,7 +1,5 @@
 package it.polimi.deib.provaFinale.alessandro_cantini_alessandro_dignani.model;
 
-import java.util.ArrayList;
-
 public class Animale {
 	private Territorio posizione;
 
@@ -11,22 +9,6 @@ public class Animale {
 
 	public void muoviIn(Territorio territorio) {
 		posizione = territorio;
-	}
-
-	
-	/**
-	 * Data una lista di animali e un territorio, controlla se c'è un animale all'interno
-	 * della lista che occupa quel territorio
-	 * 
-	 * @param lista
-	 * @param territorio
-	 * @return
-	 */
-	public static boolean territorioOccupato(ArrayList<Animale> lista, Territorio territorio) {
-		for(Animale a : lista)
-			if(a.getPosizione().equals(territorio))
-				return true;
-		return false;
 	}
 
 	public Territorio getPosizione() {
--- alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/alessandro_cantini_alessandro_dignani/model/PedinaSuStrada.java
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/alessandro_cantini_alessandro_dignani/model/PedinaSuStrada.java
@@ -1,26 +1,10 @@
 package it.polimi.deib.provaFinale.alessandro_cantini_alessandro_dignani.model;
 
-import java.util.ArrayList;
-
 public class PedinaSuStrada {
 	private Strada strada;
 
 	public PedinaSuStrada(Strada strada) {
 		this.strada = strada;
-	}
-	
-	/**
-	 * Data una strada ed una lista di pedine su strada controlla se la strada è occupata da una di quelle pedine
-	 * 
-	 * @param lista
-	 * @param strada
-	 * @return
-	 */
-	public static boolean stradaOccupata(ArrayList<PedinaSuStrada> lista, Strada strada) {
-		for(PedinaSuStrada p : lista)
-			if(p.getStrada().equals(strada))
-				return true;
-		return false;
 	}
 
 	public Strada getStrada() {
