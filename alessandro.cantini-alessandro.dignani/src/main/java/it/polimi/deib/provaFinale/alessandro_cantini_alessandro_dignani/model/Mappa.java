30dc255b1d2cd21a5024944aac1883835946e1af
alessandrocantini <alessandro.cantini@mail.polimi.it>
2014-05-18 12:27:23 +0200

    Riordinati i metodi di Mappa

--- alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/alessandro_cantini_alessandro_dignani/model/Mappa.java
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/alessandro_cantini_alessandro_dignani/model/Mappa.java
@@ -18,41 +18,6 @@ public class Mappa {
 	}
 
 	/**
-	 * Metodo di servizio che crea un array di territori il cui codice è
-	 * l'indice dell'array in @Costanti.MAPPA, il tipo viene letto in
-	 * @Costanti.TERRITORIO_CODICE
-	 * 
-	 * @return l'array di territori
-	 */
-	private Territorio[] creaTerritori() {
-		Territorio[] temp = new Territorio[Costanti.NUM_TERRITORI];
-
-		for (int i = 0; i < Costanti.NUM_TERRITORI; i++) {
-			temp[i] = new Territorio(i, Costanti.TERRITORIO_CODICE[i]);
-		}
-
-		return temp;
-	}
-
-	/**
-	 * Metodo di servizio che restituisce un array di strade creato a partire
-	 * dai territori
-	 * 
-	 * @return l'array di strade
-	 */
-	private Strada[] creaStrade() {
-		ArrayList<Strada> temp = new ArrayList<Strada>();
-		for (int i = 0; i < Costanti.NUM_TERRITORI; i++) {
-			for (int j = i + 1; j < Costanti.NUM_TERRITORI; j++) {
-				if (sonoConfinanti(territori[i], territori[j]))
-					temp.add(new Strada(territori[i], territori[j]));
-			}
-		}
-
-		return temp.toArray(new Strada[temp.size()]);
-	}
-
-	/**
 	 * Controlla se due territori sono confinanti, controllando se ci il codice
 	 * di t2 e' contenuto nella riga di t1 della matrice di transizione
 	 * Costanti.MAPPA
@@ -87,6 +52,24 @@ public class Mappa {
 	}
 	
 	/**
+	 * Metodo di servizio che restituisce un array di strade creato a partire
+	 * dai territori
+	 * 
+	 * @return l'array di strade
+	 */
+	private Strada[] creaStrade() {
+		ArrayList<Strada> temp = new ArrayList<Strada>();
+		for (int i = 0; i < Costanti.NUM_TERRITORI; i++) {
+			for (int j = i + 1; j < Costanti.NUM_TERRITORI; j++) {
+				if (sonoConfinanti(territori[i], territori[j]))
+					temp.add(new Strada(territori[i], territori[j]));
+			}
+		}
+	
+		return temp.toArray(new Strada[temp.size()]);
+	}
+
+	/**
 	 * Dati un territorio e una strada che lo delimita, restituisce il territorio opposto
 	 * a quella strada
 	 * @param t
@@ -102,8 +85,7 @@ public class Mappa {
 		
 		throw new IllegalArgumentException("Il territorio e la strada devono essere vicini");	
 	}
-	
-	
+
 	/**
 	 * Date due strade, restituisce il territorio in comune, null se non ce l'hanno
 	 * @param s1
@@ -121,7 +103,24 @@ public class Mappa {
 		
 		return null;
 	}
+
+	/**
+	 * Metodo di servizio che crea un array di territori il cui codice è
+	 * l'indice dell'array in @Costanti.MAPPA, il tipo viene letto in
+	 * @Costanti.TERRITORIO_CODICE
+	 * 
+	 * @return l'array di territori
+	 */
+	private Territorio[] creaTerritori() {
+		Territorio[] temp = new Territorio[Costanti.NUM_TERRITORI];
 	
+		for (int i = 0; i < Costanti.NUM_TERRITORI; i++) {
+			temp[i] = new Territorio(i, Costanti.TERRITORIO_CODICE[i]);
+		}
+	
+		return temp;
+	}
+
 	public static Mappa getMappa() {
 		if (istanza == null)
 			istanza = new Mappa();
