4c1cb4184c9d7e9115fd6be9811d0b97f9785f82
alessandrocantini <alessandro.cantini@mail.polimi.it>
2014-05-27 00:57:13 +0200

    Modificato PartitaTest (adattato al nuovo costruttore)

---
+++ alessandro.cantini-alessandro.dignani/src/test/java/model/PartitaTest.java
@@ -0,0 +1,51 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.ColorePastore;
+import model.Partita;
+import model.Pastore;
+import model.Strada;
+import model.Territorio;
+import model.TipoTerritorio;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PartitaTest {
+	Partita partita;
+	Strada s1, s2, s3;
+	Territorio t0, t1, t4, t7;
+	List<String> nomi;
+	
+	@Before
+	public void setUp() {		
+		t0 = new Territorio(0, TipoTerritorio.SHEEPSBURG);
+		t1 = new Territorio(1, TipoTerritorio.MONTAGNA);
+		t4 = new Territorio(4, TipoTerritorio.DESERTO);
+		t7 = new Territorio(7, TipoTerritorio.LAGO);
+		
+		s1 = new Strada(t0, t1);
+		s2 = new Strada(t0, t4);
+		s3 = new Strada(t0, t7);
+		
+		nomi = new ArrayList<String>();
+		nomi.add("esempio");
+		
+		partita = new Partita(nomi);
+		
+		partita.getRecinti().aggiungi(s1);
+		partita.getGiocatori().get(0).aggiungiPastore(new Pastore(s2, ColorePastore.BLU));
+	}
+
+	@Test
+	public void testStradaLibera() {
+		assertTrue(partita.stradaLibera(s3));
+		assertFalse(partita.stradaLibera(s1));
+		assertFalse(partita.stradaLibera(s2));
+	}
+
+}
