50651024fabea4f6a7cc030f3ff88553378c7a9e
alessandrocantini <alessandro.cantini@mail.polimi.it>
2014-05-27 15:29:13 +0200

    Creata classe Costanti in view

---
+++ alessandro.cantini-alessandro.dignani/src/main/java/view/Costanti.java
@@ -0,0 +1,14 @@
+package view;
+
+import java.io.PrintStream;
+
+public class Costanti {
+	/**
+	 * Costruttore provato per nascondere quello di default
+	 */
+	private Costanti() {
+	}
+	
+	public static PrintStream OUTPUT = System.out;
+	
+}
