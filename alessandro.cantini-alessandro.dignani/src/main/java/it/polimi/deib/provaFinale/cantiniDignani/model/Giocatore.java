23376a1b9ed12075a153c9996ae489d174cb7960
alessandrocantini <alessandro.cantini@mail.polimi.it>
2014-05-29 12:43:45 +0200

    Migliorata documentazione Giocatore, aggiunto relativo test

--- alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/model/Giocatore.java
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/model/Giocatore.java
@@ -13,32 +13,72 @@ public class Giocatore {
 	public Giocatore(String nome) {
 		this.nome = nome;
 		this.denaro = 0;
-		
+
 		pastori = new ArrayList<Pastore>();
 		tessere = new ArrayList<Tessera>();
 	}
 
+	/**
+	 * Aggiunge denaro al giocatore
+	 * 
+	 * @param quantita
+	 *            la quantita' di denaro da aggiungere
+	 */
 	public void aggiungiDenaro(int quantita) {
 		denaro += quantita;
 	}
 
+	/**
+	 * Sottrae denaro al giocatore
+	 * 
+	 * @param quantita
+	 *            la quantita' di denaro da sottrarre
+	 * @throws DenaroInsufficienteException
+	 *             se il pastore non dispone di abbastanza denaro
+	 */
+	public void sottraiDenaro(int quantita) throws DenaroInsufficienteException {
+		if (quantita > denaro) {
+			throw new DenaroInsufficienteException();
+		}
+		denaro -= quantita;
+	}
+
+	/**
+	 * Aggiunge una tessera al giocatore
+	 * 
+	 * @param tessera
+	 *            la tessera da aggiungere
+	 */
 	public void aggiungiTessera(Tessera tessera) {
 		tessere.add(tessera);
 	}
 
-	public void aggiungiPastore(Pastore p) throws RuntimeException{
-		if(pastori.size() >= Costanti.NUM_PASTORI_DUE_GIOCATORI)
-			throw new RuntimeException("Il numero di pastori non puo' essere maggiore di "+Costanti.NUM_PASTORI_DUE_GIOCATORI);
-		pastori.add(p);
+	/**
+	 * Aggiunge un pastore
+	 * 
+	 * @param pastore
+	 *            il pastore da aggiungere
+	 */
+	public void aggiungiPastore(Pastore pastore) {
+		pastori.add(pastore);
 	}
-	
 
+	/**
+	 * Restituisce una copia delle tessere
+	 * 
+	 * @return una copia dell'ArrayList delle tessere possedute dal giocatore
+	 */
 	public ArrayList<Tessera> getTessere() {
 		return Utilita.copia(tessere);
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	/**
+	 * Restituisce una copia dei pastori
+	 * 
+	 * @return una copia dell'ArrayList delle tessere possedute dal giocatore
+	 */
+	public ArrayList<Pastore> getPastori() {
+		return Utilita.copia(pastori);
 	}
 
 	public String getNome() {
@@ -49,13 +89,13 @@ public class Giocatore {
 		return this.denaro;
 	}
 
-	public ArrayList<Pastore> getPastori() {
-		return Utilita.copia(pastori);
-	}
-
-
+	/**
+	 * Eccezione che segnala che il denaro del pastore e' insufficiente per
+	 * effettuare un'operazione che richiede un costo
+	 */
 	public class DenaroInsufficienteException extends IllegalArgumentException {
-		private static final long serialVersionUID = 1L;
+
+		private static final long serialVersionUID = -270481645032368246L;
 
 		public DenaroInsufficienteException() {
 			super();
