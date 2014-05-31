e82d7fb91acbe451d278ca52f8454497567eea76
alessandrocantini <alessandro.cantini@mail.polimi.it>
2014-05-31 17:59:53 +0200

    Migliorata documentazione, metodo numeroCasuale(int min, int max) reso pubblico

--- alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/controller/Sorte.java
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/controller/Sorte.java
@@ -3,20 +3,48 @@ package it.polimi.deib.provaFinale.cantiniDignani.controller;
 import java.util.Random;
 
 public class Sorte {
+	/**
+	 * Costruttore privato per nascondere quello pubblico di default
+	 */
 	private Sorte() {
 	}
 
+	/**
+	 * Restituisce un numero casuale fra 1 e 6 compresi
+	 * 
+	 * @return un numero casuale fra 1 e 6 compresi
+	 */
 	public static int lanciaDado() {
-		return numeroCasuale (1, 6);
-    }
-	
+		return numeroCasuale(1, 6);
+	}
+
+	/**
+	 * Restituisce casualmente true o false
+	 * 
+	 * @return true o false casualmente
+	 */
 	public static boolean lanciaMoneta() {
-		return numeroCasuale(0, 1) == 0 ? false : true;
+		return new Random().nextBoolean();
+		// return numeroCasuale(0, 1) == 0 ? false : true;
 	}
-	
-	private static int numeroCasuale (int min, int max){
-		Random r = new Random();
-	
-		return r.nextInt(max-min+1) + min;
+
+	/**
+	 * Restituisce un numero casuale compreso fra i due parametri, parametri
+	 * compresi
+	 * 
+	 * @param min
+	 *            il valore minimo
+	 * @param max
+	 *            il valore massimo
+	 * @return un numero casuale compreso fra
+	 * @throws IllegalArgumentException se min e' maggiore o uguale a max
+	 */
+	public static int numeroCasuale(int min, int max) throws IllegalArgumentException {
+		if (min >= max) {
+			throw new IllegalArgumentException("il primo parametro deve essere minore del secondo");
+		}
+
+		return new Random().nextInt(max - min + 1) + min;
 	}
+
 }
