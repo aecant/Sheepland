45c89b32effd02342540e8700dca55ac4c3cd1b2
alessandrocantini <alessandro.cantini@mail.polimi.it>
2014-05-31 12:31:43 +0200

    Migliorata documentazione InsiemeDiRecinti e InsiemeDiRecintiTest

--- alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/model/InsiemeDiRecinti.java
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/model/InsiemeDiRecinti.java
@@ -1,7 +1,12 @@
 package it.polimi.deib.provaFinale.cantiniDignani.model;
 
+import it.polimi.deib.provaFinale.cantiniDignani.controller.Utilita;
+
 import java.util.ArrayList;
 
+/**
+ * Classe che fornisce i metodi per la gestione dei recinti
+ */
 public class InsiemeDiRecinti {
 	private ArrayList<PedinaSuStrada> recintiIniziali;
 	private ArrayList<PedinaSuStrada> recintiFinali;
@@ -10,29 +15,50 @@ public class InsiemeDiRecinti {
 		recintiIniziali = new ArrayList<PedinaSuStrada>();
 		recintiFinali = new ArrayList<PedinaSuStrada>();
 	}
-	
+
+	/**
+	 * Aggiunge un recinto su una strada
+	 * 
+	 * @param posizione
+	 *            la strada su cui viene aggiunto il recinto
+	 */
 	public void aggiungi(Strada posizione) {
-		if (recintiIniziali.size() < Costanti.NUM_RECINTI_INIZIALI)
+		if (recintiIniziali.size() < Costanti.NUM_RECINTI_INIZIALI) {
 			recintiIniziali.add(new PedinaSuStrada(posizione));
-		else
+		} else {
 			recintiFinali.add(new PedinaSuStrada(posizione));
+		}
 	}
-	
+
+	/**
+	 * Costruisce e restituisce la lista di tutti i recinti, iniziali e finali
+	 * 
+	 * @return la lista di tutti i recinti
+	 */
 	public ArrayList<PedinaSuStrada> getListaRecinti() {
 		ArrayList<PedinaSuStrada> recinti = new ArrayList<PedinaSuStrada>();
 		recinti.addAll(recintiIniziali);
 		recinti.addAll(recintiFinali);
-		
+
 		return recinti;
 	}
 
+	/**
+	 * Restituisce una copia dei recinti iniziali
+	 * 
+	 * @return una copia della la lista dei reciniti iniziali
+	 */
 	public ArrayList<PedinaSuStrada> getRecintiIniziali() {
-		return recintiIniziali;
+		return Utilita.copia(recintiIniziali);
 	}
 
+	/**
+	 * Restituisce una copia dei recinti finali
+	 * 
+	 * @return una copia della la lista dei reciniti finali
+	 */
 	public ArrayList<PedinaSuStrada> getRecintiFinali() {
-		return recintiFinali;
+		return Utilita.copia(recintiFinali);
 	}
-	
-	
+
 }
--- alessandro.cantini-alessandro.dignani/src/test/java/it/polimi/deib/provaFinale/cantiniDignani/model/InsiemeDiRecintiTest.java
+++ alessandro.cantini-alessandro.dignani/src/test/java/it/polimi/deib/provaFinale/cantiniDignani/model/InsiemeDiRecintiTest.java
@@ -1,33 +1,31 @@
 package it.polimi.deib.provaFinale.cantiniDignani.model;
 
-import static org.junit.Assert.*;
-import it.polimi.deib.provaFinale.cantiniDignani.model.Costanti;
-import it.polimi.deib.provaFinale.cantiniDignani.model.InsiemeDiRecinti;
-import it.polimi.deib.provaFinale.cantiniDignani.model.Mappa;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class InsiemeDiRecintiTest {
 	InsiemeDiRecinti recinti;
-	
-	/**
-	 * Inserisco un numero di recinti tali da riempire l'arraylist di recinti iniziali e meta' dei
-	 * recinti finali, in seguito controllo che le dimensioni siano corrette
-	 */
+
 	@Before
 	public void setUp() {
 		recinti = new InsiemeDiRecinti();
-		for(int i = 0; i < Costanti.NUM_RECINTI_INIZIALI + Costanti.NUM_RECINTI_FINALI / 2; i++)
-			recinti.aggiungi(Mappa.getMappa().getStrade()[i]);
 	}
-	
-	
+
+	/**
+	 * Inserisco un numero di recinti tali da riempire l'arraylist di recinti
+	 * iniziali e meta' dei recinti finali, in seguito controllo che le
+	 * dimensioni siano corrette. Le strade sono prese da @link{Mappa}
+	 */
 	@Test
 	public void testAggiungi() {
-		assertTrue(recinti.getRecintiIniziali().size() == Costanti.NUM_RECINTI_INIZIALI);
-		assertTrue(recinti.getRecintiFinali().size() == Costanti.NUM_RECINTI_FINALI / 2);
-		assertTrue(recinti.getListaRecinti().size() == Costanti.NUM_RECINTI_INIZIALI + Costanti.NUM_RECINTI_FINALI / 2);
+		for (int i = 0; i < Costanti.NUM_RECINTI_INIZIALI + Costanti.NUM_RECINTI_FINALI / 2; i++) {
+			recinti.aggiungi(Mappa.getMappa().getStrade()[i]);
+		}
+		assertEquals(recinti.getRecintiIniziali().size(), Costanti.NUM_RECINTI_INIZIALI);
+		assertEquals(recinti.getRecintiFinali().size(), Costanti.NUM_RECINTI_FINALI / 2);
+		assertEquals(recinti.getListaRecinti().size(), Costanti.NUM_RECINTI_INIZIALI + Costanti.NUM_RECINTI_FINALI / 2);
 	}
 
 }
