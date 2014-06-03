47498910b411dcfb735f3bd3ac7eaaab34378b27
alessandrocantini <alessandro.cantini@mail.polimi.it>
2014-06-03 15:43:57 +0200

    Nuova classe astratta Richiesta in eventi

---
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/controller/eventi/Richiesta.java
@@ -0,0 +1,28 @@
+package it.polimi.deib.provaFinale.cantiniDignani.controller.eventi;
+
+import it.polimi.deib.provaFinale.cantiniDignani.controller.ClientMain;
+
+/**
+ * Rappresenta un particolare tipo di evento che presuppone una risposta da parte del client.
+ *
+ */
+
+public abstract class Richiesta implements Evento {
+
+	private static final long serialVersionUID = 8125665824548264902L;
+
+	final public void aggiornaDati() {
+		;
+	}
+
+	final public void visualizza() {
+		Mossa risposta = interagisci();
+		rispondi(risposta);
+	}
+
+	private void rispondi(Mossa mossa) {
+		ClientMain.getConnessione().inviaMossa(mossa);
+	}
+
+	abstract protected Mossa interagisci();
+}
