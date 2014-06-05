dd09b4006689c4d85143626d6db96aaa6e9ab9fb
alessandrocantini <alessandro.cantini@mail.polimi.it>
2014-06-05 14:19:42 +0200

    Aggiunta classe Costanti in controller

---
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/controller/Costanti.java
@@ -0,0 +1,11 @@
+package it.polimi.deib.provaFinale.cantiniDignani.controller;
+
+public class Costanti {
+	/**
+	 * Costruttore privato per nascondere quello di default
+	 */
+	private Costanti() {
+	}
+	
+	public static final int SECONDI_TIMER = 10;
+}
