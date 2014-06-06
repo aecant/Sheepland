ab29a4835ac3a1a655b503cbb3d3ba43b54f15eb
alessandrocantini <alessandro.cantini@mail.polimi.it>
2014-06-06 17:03:46 +0200

    Aggiornato GestorePartitaTest

--- alessandro.cantini-alessandro.dignani/src/test/java/it/polimi/deib/provaFinale/cantiniDignani/controller/GestorePartitaTest.java
+++ alessandro.cantini-alessandro.dignani/src/test/java/it/polimi/deib/provaFinale/cantiniDignani/controller/GestorePartitaTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import it.polimi.deib.provaFinale.cantiniDignani.model.Costanti;
 import it.polimi.deib.provaFinale.cantiniDignani.model.Giocatore;
 import it.polimi.deib.provaFinale.cantiniDignani.model.Partita;
+import it.polimi.deib.provaFinale.cantiniDignani.model.Tessera;
 
 import java.lang.reflect.*;
 import java.util.ArrayList;
@@ -17,50 +18,83 @@ public class GestorePartitaTest {
 	List<String> listaNomi = new ArrayList<String>();
 	Partita partita;
 	GestorePartita gestore;
-	
+
 	@Before
 	public void setUp() throws NoSuchMethodException, SecurityException, NoSuchFieldException, IllegalAccessException {
-		Collections.addAll(listaNomi, "esempio1", "esempio2", "esempio3");
-		partita = new Partita(listaNomi);		
+		Collections.addAll(listaNomi, "esempio1", "esempio2", "esempio3", "esempio4");
+		partita = new Partita(listaNomi);
 		gestore = new GestorePartita(partita, null, null);
-		
+
 		Field fieldPartita = GestorePartita.class.getDeclaredField("partita");
 		fieldPartita.setAccessible(true);
 		fieldPartita.set(gestore, partita);
 	}
-	
-	
+
+	/**
+	 * Controlla che i denari siano correttamente distribuiti per ogni
+	 * configurazione di giocatori
+	 */
 	@Test
 	public void testDistribuisciDenari() throws NoSuchMethodException, SecurityException, IllegalAccessException, InvocationTargetException {
 		Method method = GestorePartita.class.getDeclaredMethod("distribuisciDenari", (Class<?>[]) null);
 		method.setAccessible(true);
-		method.invoke(gestore, (Object[]) null);
-		
-		for(Giocatore g : partita.getGiocatori()) {
-			assertEquals(g.getDenaro(), Costanti.DENARO_INIZIALE);
+
+		for (int i = Costanti.NUM_MAX_GIOCATORI; i >= Costanti.NUM_MIN_GIOCATORI; i--) {
+			partita = new Partita(listaNomi);
+			gestore = new GestorePartita(partita, null, null);
+			method.invoke(gestore, (Object[]) null);
+
+			for (Giocatore g : partita.getGiocatori()) {
+				
+				if (partita.getGiocatori().size() == 2) {
+					assertEquals(g.getDenaro(), Costanti.DENARO_INIZIALE_DUE_GIOCATORI);
+				} else {
+					assertEquals(g.getDenaro(), Costanti.DENARO_INIZIALE);
+				}
+			}
+			
+			listaNomi.remove(0);
 		}
-		
-		listaNomi.remove(0);
-		partita = new Partita(listaNomi);
-		gestore = new GestorePartita(partita, null, null);
-		method.invoke(gestore, (Object[]) null);
-		
-		for(Giocatore g : partita.getGiocatori()) {
-			assertEquals(g.getDenaro(), Costanti.DENARO_INIZIALE_DUE_GIOCATORI);
-		}
+
 	}
-	
+
+	/**
+	 * Controlla che ogni territorio abbia esattamente una pecora, un montone o
+	 * un agnello
+	 */
 	@Test
 	public void disponiPecora() throws NoSuchMethodException, SecurityException, IllegalAccessException, InvocationTargetException {
 		Method method = GestorePartita.class.getDeclaredMethod("disponiPecore", (Class<?>[]) null);
 		method.setAccessible(true);
 		method.invoke(gestore, (Object[]) null);
-		
+
 		DatiTerritorio[] dati = Estrattore.datiTerritori(partita);
-		
-		for(DatiTerritorio d : dati) {
+
+		for (DatiTerritorio d : dati) {
 			assertEquals(d.getNumOvini(), 1);
 		}
+	}
+
+	/**
+	 * Controlla che dopo aver disposto le tessere iniziali ogni giocatore abbia
+	 * esattamente una tessera e che non siano state distribuite tessere
+	 * duplicate
+	 */
+	@Test
+	public void testDisponiTessereIniziali() throws NoSuchMethodException, SecurityException, IllegalAccessException, InvocationTargetException {
+		Method method = GestorePartita.class.getDeclaredMethod("disponiTessereIniziali", (Class<?>[]) null);
+		method.setAccessible(true);
+		method.invoke(gestore, (Object[]) null);
+
+		List<Tessera> listaTessere = new ArrayList<Tessera>();
+
+		for (Giocatore g : partita.getGiocatori()) {
+			assertEquals(g.getTessere().size(), 1);
+			listaTessere.addAll(g.getTessere());
+		}
+
+		assertFalse(Utilita.contieneDuplicati(listaTessere));
+
 	}
 
 }
