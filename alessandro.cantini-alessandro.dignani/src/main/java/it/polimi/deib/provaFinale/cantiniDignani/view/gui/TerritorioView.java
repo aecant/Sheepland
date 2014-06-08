7fcc97839e84305b41d3b4156e29d7582b85b460
alessandrocantini <alessandro.cantini@mail.polimi.it>
2014-06-08 23:37:14 +0200

    Aggiunta classe TerritorioView

---
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/view/gui/TerritorioView.java
@@ -0,0 +1,49 @@
+package it.polimi.deib.provaFinale.cantiniDignani.view.gui;
+
+import it.polimi.deib.provaFinale.cantiniDignani.controller.DatiTerritorio;
+import it.polimi.deib.provaFinale.cantiniDignani.model.TipoAnimale;
+
+import java.awt.Point;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TerritorioView {
+	private Map<TipoAnimale, Point> coordAnimali;
+	private final Point[] coordinate;
+	private final int codTerr;
+	private DatiTerritorio dati;
+	private int indice;
+
+	public TerritorioView(int codTerr, Point[] coordinate, DatiTerritorio dati) {
+		this.codTerr = codTerr;
+		this.coordinate = coordinate;
+		this.dati = dati;
+		aggiorna();
+	}
+
+	/**
+	 * Aggiorna la mappa delle coordinate degli animali
+	 */
+	public void aggiorna() {
+		coordAnimali = new HashMap<TipoAnimale, Point>();
+		indice = 0;
+		for (TipoAnimale tipo : TipoAnimale.values()) {
+			if (dati.getTipiOvino().contains(tipo)) {
+				coordAnimali.put(tipo, coordinate[indice]);
+				indice++;
+			}
+		}
+	}
+
+	public Point getCoordinate(TipoAnimale tipo) {
+		return coordAnimali.containsKey(tipo) ? coordAnimali.get(tipo) : coordinate[indice];
+	}
+
+	public int getCodTerr() {
+		return codTerr;
+	}
+
+	public DatiTerritorio getDati() {
+		return dati;
+	}
+}
