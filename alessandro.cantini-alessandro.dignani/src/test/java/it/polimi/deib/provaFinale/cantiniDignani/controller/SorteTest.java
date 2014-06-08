fa2e1e91cdc336d4e691762882299554bbe346f2
alessandrocantini <alessandro.cantini@mail.polimi.it>
2014-06-08 14:37:59 +0200

    Migliorato SorteTest

--- alessandro.cantini-alessandro.dignani/src/test/java/it/polimi/deib/provaFinale/cantiniDignani/controller/SorteTest.java
+++ alessandro.cantini-alessandro.dignani/src/test/java/it/polimi/deib/provaFinale/cantiniDignani/controller/SorteTest.java
@@ -2,6 +2,10 @@ package it.polimi.deib.provaFinale.cantiniDignani.controller;
 
 import static org.junit.Assert.*;
 import it.polimi.deib.provaFinale.cantiniDignani.controller.Sorte;
+import it.polimi.deib.provaFinale.cantiniDignani.model.Agnello;
+import it.polimi.deib.provaFinale.cantiniDignani.model.Pecora;
+import it.polimi.deib.provaFinale.cantiniDignani.model.Territorio;
+import it.polimi.deib.provaFinale.cantiniDignani.model.TipoTerritorio;
 
 import org.junit.Before;
 import org.junit.Ignore;
@@ -12,6 +16,7 @@ public class SorteTest {
 	private final int DELTA = NUMERO_LANCI / 50;
 	Integer contTrue, contFalse;
 	Integer[] contDado;
+	Territorio terr = new Territorio(0, TipoTerritorio.SHEEPSBURG);
 
 	@Before
 	public void setUpMoneta() {
@@ -27,9 +32,9 @@ public class SorteTest {
 	@Before
 	public void setUpDado() {
 		contDado = new Integer[6];
-		for(int i = 0; i < contDado.length; i++)
+		for (int i = 0; i < contDado.length; i++)
 			contDado[i] = new Integer(0);
-		
+
 		for (int i = 0; i < NUMERO_LANCI; i++)
 			contDado[Sorte.lanciaDado() - 1]++;
 	}
@@ -42,16 +47,26 @@ public class SorteTest {
 		for (int i = 0; i < NUMERO_LANCI; i++) {
 			int lancio = Sorte.lanciaDado();
 			assertTrue(lancio >= 1 && lancio <= 6);
-		}		
+		}
 	}
-	
-	@Test 
+
+	@Test
+	public void testPecoraRandom() {
+		assertTrue(Sorte.pecoraRandom(terr) instanceof Pecora);
+	}
+
+	@Test
+	public void testAgnelloRandom() {
+		assertTrue(Sorte.pecoraRandom(terr) instanceof Agnello);
+	}
+
+	@Test
 	@Ignore("Test non deterministico")
-	public void testEquitaDado(){
-		for(Integer cont : contDado)
+	public void testEquitaDado() {
+		for (Integer cont : contDado)
 			assertEquals(cont, NUMERO_LANCI / 6, DELTA);
 	}
-	
+
 	@Test
 	@Ignore("Test non deterministico")
 	public void testEquitaMoneta() {
