15f91ba63f866e51a7be5aeb0215f158d368a546
alessandrodignani <alessandro.dignani@mail.polimi.it>
2014-06-09 18:52:51 +0200

    Impostate costanti relative rispetto alla dimensione della mappa

--- alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/view/gui/CostantiGui.java
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/view/gui/CostantiGui.java
@@ -3,6 +3,7 @@ package it.polimi.deib.provaFinale.cantiniDignani.view.gui;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Point;
+import java.awt.Toolkit;
 
 public class CostantiGui {
 
@@ -15,21 +16,26 @@ public class CostantiGui {
 	 */
 	private CostantiGui() {
 	}
-		
+
 	public static String percorsoImmagini = new String("immagini/");
 	
 	public static Color coloreAcqua = new Color(34, 145, 243);
 	
 	// Dimensioni delle immagini disegnate
-	public static Dimension dimensionePecora = new Dimension(40, 30);
-	public static Dimension dimensioneMontone = new Dimension(40, 30);
-	public static Dimension dimensioneAgnello = new Dimension(40, 30);
-	public static Dimension dimensioneLupo = new Dimension(40, 30);
-	public static Dimension dimensioneMappa = new Dimension(500, 700);	
+	public static final Dimension dimensioneMappa;
+	static {
+		dimensioneMappa = new Dimension((int) (Toolkit.getDefaultToolkit().getScreenSize().height*(0.75)), Toolkit.getDefaultToolkit().getScreenSize().height);
+	}
+	public static final Dimension dimensionePecora = new Dimension(dimensioneMappa.height/15, dimensioneMappa.height/20);
+	public static final Dimension dimensioneMontone = new Dimension(dimensioneMappa.height/15, dimensioneMappa.height/20);
+	public static final Dimension dimensioneAgnello = new Dimension(dimensioneMappa.height/15, dimensioneMappa.height/20);
+	public static final Dimension dimensioneLupo = new Dimension(dimensioneMappa.height/15, dimensioneMappa.height/20);
 	
-	public static int dimensioneAngolini = 20;
 	
+	public static final Dimension dimensionePanelTessera = new Dimension(dimensioneMappa.height/6, dimensioneMappa.height/6);
+	
+	public static final int dimensioneAngolini = dimensioneMappa.height/(6*5);
 
 	// Coordinate delle posizioni delle pecore nei territori (5 per ogni territorio)
-	public static Point[][] coordinate = {};
+	public static final Point[][] coordinate = {};
 }
