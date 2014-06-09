b63d2df5b3145286d38955cf403b3543c255f2f0
alessandrodignani <alessandro.dignani@mail.polimi.it>
2014-06-09 18:49:00 +0200

    Aggiornati i metodi di disegno del background

--- alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/view/gui/BackgroundMappaPanel.java
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/view/gui/BackgroundMappaPanel.java
@@ -20,6 +20,6 @@ public class BackgroundMappaPanel extends JPanel {
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.drawImage(img, 0, 0, (int)(this.getHeight()*0.75), this.getHeight(), this);
+		g.drawImage(img, (int) ((this.getWidth()-(this.getHeight()*0.75))/2), 0, (int)(this.getHeight()*0.75), this.getHeight(), this);
 	}
 }
--- alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/view/gui/BackgroundPanel.java
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/view/gui/BackgroundPanel.java
@@ -19,6 +19,6 @@ public class BackgroundPanel extends JPanel {
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.drawImage(img, 0, 0, this);
+		g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
 	}
 }
