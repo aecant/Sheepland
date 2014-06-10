3babf2ae07adc257d90909b3bc1bf8482114c4b1
alessandrodignani <alessandro.dignani@mail.polimi.it>
2014-06-10 22:45:56 +0200

    aggiunta una stringa descrittiva del tipo di mossa

--- alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/controller/TipoMossa.java
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/controller/TipoMossa.java
@@ -1,9 +1,15 @@
 package it.polimi.deib.provaFinale.cantiniDignani.controller;
 
 public enum TipoMossa {
-	MUOVI_PASTORE,
-	MUOVI_PECORA,
-	ACQUISTA_TESSERA,
-	ACCOPPIA,
-	ABBATTI;
+	MUOVI_PASTORE("Muovi pastore"),
+	MUOVI_PECORA("Muovi pecora"),
+	ACQUISTA_TESSERA("Acquista tessera"),
+	ACCOPPIA("Accoppia"),
+	ABBATTI("Abbatti");
+	
+	public final String nome;
+	
+	private TipoMossa(String nome) {
+		this.nome = nome;
+	}
 }
