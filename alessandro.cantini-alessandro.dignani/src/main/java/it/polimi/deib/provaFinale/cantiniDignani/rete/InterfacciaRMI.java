fed8785cbe515d24c844b68efdbc366cdad449b3
alessandrocantini <alessandro.cantini@mail.polimi.it>
2014-06-10 15:43:25 +0200

    Eliminato metodo inutile tessereConfinanti in InterfacciaRMI e InterfacciaRMIImpl

--- alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/rete/InterfacciaRMI.java
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/rete/InterfacciaRMI.java
@@ -1,14 +1,11 @@
 package it.polimi.deib.provaFinale.cantiniDignani.rete;
 
 import it.polimi.deib.provaFinale.cantiniDignani.controller.eventi.Mossa;
-import it.polimi.deib.provaFinale.cantiniDignani.model.Tessera;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface InterfacciaRMI extends Remote {
-
-	public Tessera[] getTessereConfinanti(String giocatore, int strada) throws RemoteException;
 
 	public void registraGiocatore(String giocatore) throws RemoteException;
 
--- alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/rete/InterfacciaRMIImpl.java
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/rete/InterfacciaRMIImpl.java
@@ -1,17 +1,11 @@
 package it.polimi.deib.provaFinale.cantiniDignani.rete;
 
-import it.polimi.deib.provaFinale.cantiniDignani.controller.Estrattore;
 import it.polimi.deib.provaFinale.cantiniDignani.controller.ServerMain;
 import it.polimi.deib.provaFinale.cantiniDignani.controller.eventi.Mossa;
-import it.polimi.deib.provaFinale.cantiniDignani.model.Tessera;
 
 import java.rmi.RemoteException;
 
 public class InterfacciaRMIImpl implements InterfacciaRMI {
-
-	public Tessera[] getTessereConfinanti(String giocatore, int strada) throws RemoteException {
-		return Estrattore.tessereConfinanti(ServerMain.getPartita(giocatore), strada);
-	}
 
 	public void registraGiocatore(String nome) throws NomeGiaPresenteException, RemoteException {
 		if (!ServerMain.aggiungiGiocatore(nome)) {
