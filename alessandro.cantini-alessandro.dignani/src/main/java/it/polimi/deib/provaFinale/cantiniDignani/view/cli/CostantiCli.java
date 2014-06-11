bcb21ba2065657453704f13d602a4ed3be28392b
alessandrocantini <alessandro.cantini@mail.polimi.it>
2014-06-11 23:40:08 +0200

    Sistemati piccoli dettagli in CostantiCli

--- alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/view/cli/CostantiCli.java
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/view/cli/CostantiCli.java
@@ -5,14 +5,13 @@ import java.io.PrintStream;
 
 public class CostantiCli {
 
+	public static final InputStream DEFAULT_INPUT = System.in;
+
+	public static final PrintStream DEFAULT_OUTPUT = System.out;
+
 	/**
 	 * Costruttore provato per nascondere quello di default
 	 */
 	private CostantiCli() {
 	}
-	
-	public static final InputStream DEFAULT_INPUT = System.in;
-	
-	public static PrintStream DEFAULT_OUTPUT = System.out;
-	
 }
