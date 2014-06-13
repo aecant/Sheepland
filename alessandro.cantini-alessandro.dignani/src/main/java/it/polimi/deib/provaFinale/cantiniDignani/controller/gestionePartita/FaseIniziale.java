72d703b21357ff9d9a138019ee3df7784f4b4766
alessandrocantini <alessandro.cantini@mail.polimi.it>
2014-06-13 19:12:01 +0200

    Diminuita complessità di GestorePartita delegando alcune funzionalità a FaseIniziale

---
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/controller/gestionePartita/FaseIniziale.java
@@ -0,0 +1,37 @@
+package it.polimi.deib.provaFinale.cantiniDignani.controller.gestionePartita;
+
+import it.polimi.deib.provaFinale.cantiniDignani.controller.Estrattore;
+import it.polimi.deib.provaFinale.cantiniDignani.controller.eventi.PosizionamentoPastore;
+import it.polimi.deib.provaFinale.cantiniDignani.controller.eventi.RichiestaPosizioneInizialePastore;
+import it.polimi.deib.provaFinale.cantiniDignani.model.Giocatore;
+import it.polimi.deib.provaFinale.cantiniDignani.model.Mappa;
+import it.polimi.deib.provaFinale.cantiniDignani.model.Strada;
+
+public class FaseIniziale extends FasePartita {
+
+	public FaseIniziale(GestorePartita gestore) {
+		super(gestore);
+	}
+
+	@Override
+	public void esegui() {
+		giroPosizionamentoPastore(0);
+		if (gestore.dueGiocatori) {
+			giroPosizionamentoPastore(1);
+		}
+	}
+
+	private void giroPosizionamentoPastore(int numPastore) {
+		for (Giocatore g : partita.getGiocatori()) {
+			boolean[] stradeLibere = Estrattore.stradeLibere(partita);
+			gestore.inviaEvento(new RichiestaPosizioneInizialePastore(stradeLibere), g);
+			PosizionamentoPastore risposta = (PosizionamentoPastore) gestore.aspettaEvento(PosizionamentoPastore.class);
+
+			Strada strada = Mappa.getMappa().getStrade()[risposta.getStrada()];
+			g.getPastori().get(numPastore).muoviIn(strada);
+
+			gestore.inviaEventoATutti(new PosizionamentoPastore(g.getNome(), risposta.getStrada()));
+		}
+	}
+	
+}
