c0f38845dda291c0c7781b3ec50db1c5cb2d9fb0
alessandrodignani <alessandro.dignani@mail.polimi.it>
2014-06-16 15:36:14 +0200

    or eGui implementa InterfacciaUtente

--- alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/view/gui/Gui.java
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/view/gui/Gui.java
@@ -1,20 +1,27 @@
 package it.polimi.deib.provaFinale.cantiniDignani.view.gui;
 
+import it.polimi.deib.provaFinale.cantiniDignani.controller.ClientMain;
+import it.polimi.deib.provaFinale.cantiniDignani.controller.Coppia;
 import it.polimi.deib.provaFinale.cantiniDignani.controller.DatiPartita;
-import it.polimi.deib.provaFinale.cantiniDignani.controller.Estrattore;
+import it.polimi.deib.provaFinale.cantiniDignani.controller.MotivoLancioDado;
 import it.polimi.deib.provaFinale.cantiniDignani.controller.Sorte;
+import it.polimi.deib.provaFinale.cantiniDignani.controller.TipoMossa;
 import it.polimi.deib.provaFinale.cantiniDignani.controller.gestionePartita.GestorePartita;
-import it.polimi.deib.provaFinale.cantiniDignani.controller.gestionePartita.PreparazionePartita;
 import it.polimi.deib.provaFinale.cantiniDignani.model.ColorePastore;
 import it.polimi.deib.provaFinale.cantiniDignani.model.Costanti;
 import it.polimi.deib.provaFinale.cantiniDignani.model.Partita;
+import it.polimi.deib.provaFinale.cantiniDignani.model.Tessera;
+import it.polimi.deib.provaFinale.cantiniDignani.model.TipoAnimale;
+import it.polimi.deib.provaFinale.cantiniDignani.view.InterfacciaUtente;
 
 import java.awt.Point;
-import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
-public class Gui {
+public class Gui implements InterfacciaUtente{
 	private FinestraChiediNome fcn;
 	static PartitaView finestraPartita;
+	boolean messErrore = false;
 
 	// Tempornei
 	static public Partita part;
@@ -27,7 +34,7 @@ public class Gui {
 	 * @return una String con il nome
 	 */
 	public String chiediNome() {
-		fcn = new FinestraChiediNome(false);
+		fcn = new FinestraChiediNome(messErrore);
 		return fcn.riceviNome();
 	}
 
@@ -37,9 +44,8 @@ public class Gui {
 	 * 
 	 * @return String del nome scelto
 	 */
-	public String nomeGiaPresente() {
-		fcn = new FinestraChiediNome(true);
-		return fcn.riceviNome();
+	public void nomeGiaPresente() {
+		messErrore = true;
 	}
 
 	/**
@@ -48,12 +54,12 @@ public class Gui {
 	public void inizioPartita() {
 		// TODO questo è un test, andrà mofificato con
 		// ClientMain.getDatiPartita()
-		part = new Partita(Arrays.asList("Dig", "Andre", "Luca", "Cant"));
-		gest = new GestorePartita(part, null, null);
-		PreparazionePartita fi = new PreparazionePartita(gest);
-		fi.disponiPecore();
-		fi.disponiTessereIniziali();
-		fi.distribuisciDenari();
+//		part = new Partita(Arrays.asList("Dig", "Andre", "Luca", "Cant"));
+//		gest = new GestorePartita(part, null, null);
+//		PreparazionePartita fi = new PreparazionePartita(gest);
+//		fi.disponiPecore();
+//		fi.disponiTessereIniziali();
+//		fi.distribuisciDenari();
 		
 
 		finestraPartita = new PartitaView(getDati());
@@ -78,13 +84,16 @@ public class Gui {
 			finestraPartita.getMappa().creaPastore(coordinateStrade[Sorte.numeroCasuale(0, Costanti.NUM_STRADE - 1)], ColorePastore.values()[i]);
 		}
 
-		for (int i = 0; i < Costanti.NUM_TERRITORI; i++) {
-			finestraPartita.getMappa().disegnaTerritorio(i);
-			System.out.println("Territorio " + i + getDati().getTerritori()[i].toString());
-		}
+		disegnaStatoTerritori();
 		
 		finestraPartita.visualizza();
 		finestraPartita.getPanelMessaggi().visualizzaMessaggio("La tua tessera iniziale è di tipo " + Gui.getDati().getGiocatore(Gui.getNome()).getTessere().get(0).getTipo().toString());
+	}
+
+	private void disegnaStatoTerritori() {
+		for (int i = 0; i < Costanti.NUM_TERRITORI; i++) {
+			finestraPartita.getMappa().disegnaTerritorio(i);
+		}
 	}
 
 	/**
@@ -101,25 +110,119 @@ public class Gui {
 	}
 
 	// Test
-	public static void main(String[] args) {
-		Gui gui = new Gui();
-
-		gui.inizioPartita();
-
-		gui.inizioTurno(getDati().getGiocatoreDiTurno());
-	}
+//	public static void main(String[] args) {
+//		Gui gui = new Gui();
+//
+//		gui.inizioPartita();
+//
+//		gui.inizioTurno(getDati().getGiocatoreDiTurno());
+//	}
 
 	static DatiPartita getDati() {
 
-		return Estrattore.datiPartita(part);
-		// Fine test
+		return ClientMain.getDatiPartita();
 	}
 
 	protected static String getNome() {
-		return "Dig";
+		return ClientMain.getNome();
 	}
 
 	public static PartitaView getFinestraPartita() {
 		return finestraPartita;
+	}
+
+	public void lancioDado(Integer numero, MotivoLancioDado motivo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void selezionePosizioneInizialePastore(String giocatore, int strada) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void movimentoPecora(String giocatore, TipoAnimale pecora, int origine, int destinazione) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void movimentoPecoraNera(int origine, int destinazione) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void movimentoPastore(String giocatore, int origine, int destinazione) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void movimentoLupo(int origine, int destinazione) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void acquistoTessera(String giocatore, Tessera tessera) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void abbattimento(String giocatore, TipoAnimale tipo, int territorio, boolean aBuonFine) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void accoppiamento(String giocatore, int territorio, boolean aBuonFine) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void trasformazioneAgnello(boolean maschio, Integer territorio) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void pagamento(Integer denaro, String pagante, String pagato) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int richiestaPosizioneInizialePastore(boolean[] stradeLibere) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int richiestaPosizionePastore(boolean[] stradeLibereGratis, boolean[] stradeLibereAPagamento) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int richiestaPastore() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int richiestaTerritorioPerAccoppiamento(Collection<Integer> territoriDisponibili) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int richiestaTipoMossa(List<TipoMossa> mosseDisponibili, int numMossa) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int richiestaPecoraDaMuovere(List<Coppia<Integer, TipoAnimale>> oviniSpostabili) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int richiestaPecoraDaAbbattere(List<Coppia<Integer, TipoAnimale>> oviniAbbattibili) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int richiestaTesseraDaAcquistare(List<Tessera> tessereDisponibili) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
