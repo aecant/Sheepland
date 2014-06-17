fbf8f199d775ff4437e32ba009edfc812845a445
alessandrodignani <alessandro.dignani@mail.polimi.it>
2014-06-17 23:03:49 +0200

    Aggiunta password in finestra chiedi nome

--- alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/view/gui/FinestraChiediNome.java
+++ alessandro.cantini-alessandro.dignani/src/main/java/it/polimi/deib/provaFinale/cantiniDignani/view/gui/FinestraChiediNome.java
@@ -1,19 +1,19 @@
 package it.polimi.deib.provaFinale.cantiniDignani.view.gui;
 
-import it.polimi.deib.provaFinale.cantiniDignani.view.gui.CostantiGui;
+import it.polimi.deib.provaFinale.cantiniDignani.utilita.Coppia;
+import it.polimi.deib.provaFinale.cantiniDignani.utilita.GestoreCoda;
 
 import java.awt.Color;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class FinestraChiediNome extends JFrame {
@@ -24,8 +24,9 @@ public class FinestraChiediNome extends JFrame {
 	private JTextField txtNome;
 	private JLabel lblInserisciIlNome;
 	private JLabel lblIlNomeScelto;
+	private JPasswordField passwordField;
 
-	private BlockingQueue<String> coda = new LinkedBlockingQueue<String>();
+	private GestoreCoda <Coppia<String, String>> coda = new GestoreCoda<Coppia<String, String>>();
 
 	public FinestraChiediNome(boolean visualizzaMessaggioErrore) {
 		super("Sheepland - Inserisci il nome");
@@ -37,9 +38,13 @@ public class FinestraChiediNome extends JFrame {
 
 		txtNome = new JTextField(10);
 		txtNome.setBounds(50, 130, 180, 30);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(50, 160, 180, 30);
 
 		contentPane.setLayout(null);
 		contentPane.add(txtNome);
+		contentPane.add(passwordField);
 
 		lblInserisciIlNome = new JLabel("<html>Inserisci il nome e clicca invia<br />per iniziare una nuova partita</html>");
 		lblInserisciIlNome.setBounds(50, 94, 206, 38);
@@ -60,23 +65,19 @@ public class FinestraChiediNome extends JFrame {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				// Controllo che il campo testo non sia vuoto (sarebbe meglio aggiungere altri controlli)
-				if(txtNome.getText().length() != 0) {
-					coda.add(txtNome.getText());
+				if(txtNome.getText().length() != 0 && passwordField.getPassword().toString().length() != 0) {
+					coda.aggiungi(Coppia.creaCoppia(txtNome.getText(), passwordField.getPassword().toString()));
 				}
 			}
 		});
-		btnInvia.setBounds(113, 159, 117, 29);
+		btnInvia.setBounds(113, 200, 117, 29);
 		contentPane.add(btnInvia);
 	}
 
-	public String riceviNome() {
+	public Coppia<String, String> riceviNome() {
 		setVisible(true);
-		String temp = null;
-		try {
-			temp = coda.take();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		Coppia<String, String> temp = null;
+		temp = coda.aspetta();
 		dispose();
 		return temp;
 	}
